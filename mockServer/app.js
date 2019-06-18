d3aef51d52213b81ce8acf580b05b30fc565b7de
jonhindle <jon.hindle@digital.justice.gov.uk>
2019-06-18 09:20:40 +0100

    SP-121-mock-needs-data

--- mockServer/app.js
+++ mockServer/app.js
@@ -2,21 +2,24 @@ const fs = require('fs')
 const app = require('express')()
 const logger = require('../log.js')
 
+const getFile = (directory, file, res) => {
+  const path = `mockServer/${directory}/${file}.json`
+  logger.info(`apiMockServer requesting ${path}`)
+  fs.readFile(path, (err, sentencePlan) => {
+    return err ? res.status(404).send(`Invalid path ${path}`) : res.send(JSON.parse(sentencePlan))
+  })
+}
+
 module.exports = function createMockAPI() {
   app.get('/', (req, res) => res.send('sentence-planner-mock is UP'))
   app.get(/offenders\/(crn|oasysOffenderId)\/([\w]+)$/, (req, res) => {
-    const path = `mockServer/convictData/${req.params[1]}.json`
-    logger.info(`apiMockServer requesting ${path}`)
-    fs.readFile(path, (err, convictData) => {
-      return err ? res.status(404).send('Invalid CRN') : res.send(JSON.parse(convictData))
-    })
+    return getFile('convictData', req.params[1], res)
   })
   app.get(/offenders\/(nomisId|oasysOffenderId)\/([\w]+)\/sentencePlans\/latest$/, (req, res) => {
-    const path = `mockServer/sentencePlan/${req.params[1]}.json`
-    logger.info(`apiMockServer requesting ${path}`)
-    fs.readFile(path, (err, sentencePlan) => {
-      return err ? res.status(404).send('Invalid nomisId') : res.send(JSON.parse(sentencePlan))
-    })
+    return getFile('sentencePlan', req.params[1], res)
+  })
+  app.get(/offenders\/oasysOffenderId\/([\w]+)\/assessments\/latest\/needs$/, (req, res) => {
+    return getFile('needs', req.params[0], res)
   })
   app.listen(18080)
 }
