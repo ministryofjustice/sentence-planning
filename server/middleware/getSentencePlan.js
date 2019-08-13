03b87abc56b1c775537ea9cb01151c1ae1cdd99e
jonhindle <jon.hindle@digital.justice.gov.uk>
2019-08-13 11:00:53 +0100

    SP-110-my-progress-screen

--- server/middleware/getSentencePlan.js
+++ server/middleware/getSentencePlan.js
@@ -1,47 +1,6 @@
 const logger = require('../../log')
-const { getTimeStringFromISO8601 } = require('../utils/displayHelpers')
 const { searchBreadcrumb, summaryBreadcrumb } = require('./breadcrumbHelpers')
-
-const getProgress = (progress, defaultProgress = 'In progress') => {
-  if (!progress || progress.length === 0) return defaultProgress
-  const currentProgress = progress.reduce((next, last) => (next.dateCreated > last.dateCreated ? next : last))
-    .progressStep
-  return `${currentProgress.substring(0, 1)}${currentProgress.substring(1).toLowerCase()}`.replace('_', ' ')
-}
-
-const getSentencePlanSteps = (oasysOffenderId, sentencePlanId, sentencePlans) => {
-  return sentencePlans
-    .find(({ sentencePlanId: id }) => {
-      return id === sentencePlanId
-    })
-    .steps.map(({ step = '', intervention = '', stepId, dateCreated, progress = [] }) => {
-      return {
-        step: step || intervention,
-        status: getProgress(progress),
-        lastUpdate: getTimeStringFromISO8601(dateCreated),
-        stepId,
-      }
-    })
-}
-
-const getSentencePlanPastSteps = (oasysOffenderId, sentencePlanId, sentencePlans) => {
-  try {
-    return sentencePlans
-      .find(({ sentencePlanId: id }) => {
-        return id === sentencePlanId
-      })
-      .pastSteps.map(({ step = '', intervention = '', stepId, dateCreated, progress = [] }) => {
-        return {
-          step: step || intervention,
-          status: getProgress(progress, 'completed'),
-          lastUpdate: getTimeStringFromISO8601(dateCreated),
-          stepId,
-        }
-      })
-  } catch (error) {
-    return []
-  }
-}
+const { getSentencePlanSteps, getSentencePlanPastSteps } = require('./sentencePlanHelpers')
 
 module.exports = () => async (req, res) => {
   const {
