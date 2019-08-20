acdba9c9daeccb54c3a3a4fde9b1a4c296a341d2
jonhindle <jon.hindle@digital.justice.gov.uk>
2019-08-20 14:24:24 +0100

    SP-106-SP-262-this-is-me-and-motivations-pages

---
+++ server/config/motivations.js
@@ -0,0 +1,13 @@
+module.exports = {
+  motivations: {
+    fields: [
+      {
+        motivations: {
+          responseType: 'requiredMotivations',
+          validationMessage: 'Please select a motivation for each need.',
+        },
+      },
+    ],
+    validate: true,
+  },
+}
