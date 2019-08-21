6f1ff503e08d015e9a58b2a4fd265dff03cf3e31
jonhindle <jon.hindle@digital.justice.gov.uk>
2019-08-21 14:27:27 +0100

    SP-309-breadcrumbs-for-motivations

--- server/middleware/getMotivations.js
+++ server/middleware/getMotivations.js
@@ -1,4 +1,6 @@
 const logger = require('../../log')
+const { sentencePlanChildrenBreadcrumbs } = require('./breadcrumbHelpers')
+const { getTimeStringFromISO8601 } = require('../utils/displayHelpers')
 
 module.exports = () => async (req, res) => {
   const {
@@ -7,11 +9,21 @@ module.exports = () => async (req, res) => {
   try {
     const { locals } = res
     const {
+      forename1,
+      familyName,
       formObject: { sentencePlans },
     } = locals
     locals.sentencePlanId = sentencePlanId
     locals.linkRoot = `/sentence-plan/oasys-offender-id/${oasysOffenderId}/sentence-plan/${sentencePlanId}/step/`
-    locals.sentencePlan = sentencePlans.find(({ sentencePlanId: spId }) => sentencePlanId === spId)
+    const sentencePlan = sentencePlans.find(({ sentencePlanId: spId }) => sentencePlanId === spId)
+    locals.sentencePlan = sentencePlan
+    locals.breadcrumbs = sentencePlanChildrenBreadcrumbs(
+      oasysOffenderId,
+      forename1,
+      familyName,
+      sentencePlanId,
+      sentencePlan ? getTimeStringFromISO8601(sentencePlan.dateCreated) : null
+    )
     return res.render('../views/formPages/motivations', locals)
   } catch (err) {
     logger.warn(`Could not find sentence plan: ${sentencePlanId}`)
