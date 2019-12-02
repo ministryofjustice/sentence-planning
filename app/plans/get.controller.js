c1c399bc1bcd76767c129f364cfa35eeb84454cf
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2019-12-02 11:54:56 +0000

    SP-422 Displays 'draft plan' and 'active plan' homepages

--- app/plans/get.controller.js
+++ app/plans/get.controller.js
@@ -1,27 +1,27 @@
 const { getSentencePlanSummary } = require('../../common/data/sentencePlanningApi')
 const { isEmptyObject } = require('../../common/utils/util')
 
-const hasActivePlan = plans => {
-  if (isEmptyObject(plans)) return false
-  let activePlanPresent = false
-  plans.forEach(plan => {
-    if (!plan.completedDate) {
-      activePlanPresent = true
-    }
-  })
-  return activePlanPresent
+const getPlan = plans => {
+  if (isEmptyObject(plans)) return {}
+  const [thePlan] = plans.filter(plan => plan.completedDate === '' || plan.completedDate === undefined)
+
+  return thePlan || {}
 }
 
 const sentencePlanSummary = async ({ params: { id }, session: { 'x-auth-token': token } }, res) => {
   const plans = await getSentencePlanSummary(id, token)
   const renderInfo = {}
-  if (!plans || isEmptyObject(plans)) {
-    renderInfo.activePlan = false
-  } else {
-    renderInfo.activePlan = hasActivePlan(plans)
-  }
   renderInfo.individualId = id
+  renderInfo.currentPlan = getPlan(plans)
+  if (isEmptyObject(renderInfo.currentPlan)) {
+    renderInfo.planType = 'none'
+  } else if (renderInfo.currentPlan.isDraft) {
+    renderInfo.planType = 'draft'
+  } else {
+    renderInfo.planType = 'active'
+  }
+
   res.render(`${__dirname}/index`, renderInfo)
 }
 
-module.exports = { sentencePlanSummary, hasActivePlan }
+module.exports = { sentencePlanSummary }
