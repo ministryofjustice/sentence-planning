22bd6807f9d76f0ef344817b9095a126da9f1313
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2020-02-21 16:31:45 +0000

    SP-568 print layout and contents

--- app/printing/printFull/get.controller.js
+++ app/printing/printFull/get.controller.js
@@ -1,13 +1,74 @@
 const { logger } = require('../../../common/logging/logger')
-const { removeUrlLevels } = require('../../../common/utils/util')
+const { removeUrlLevels, groupBy, getYearMonthFromDate, getStatusText } = require('../../../common/utils/util')
+const { getCommentText } = require('../../../common/utils/getCommentText')
+const { getSentencePlan } = require('../../../common/data/sentencePlanningApi')
 
-const printFullSentencePlan = async ({ path, params: { id, planId } }, res) => {
+const printFullSentencePlan = async ({ path, params: { id, planId }, tokens }, res) => {
   try {
     const backUrl = removeUrlLevels(path, 1)
-    res.render(`${__dirname}/index`, { backUrl })
+
+    let objectives
+    let comments
+
+    // get the sentence plan
+    try {
+      ;({ comments, objectives = [] } = await getSentencePlan(planId, tokens))
+    } catch (error) {
+      logger.error(`Could not retrieve active sentence plan ${planId} for offender ${id}, error: ${error}`)
+      return res.render('app/error', { error })
+    }
+
+    // determine the status of each objective
+    objectives = objectives.map(objective => {
+      const currentObjective = objective
+
+      // objectives default to active if not caught with the rules below
+      currentObjective.type = 'active'
+
+      if (currentObjective.actions.every(({ status }) => ['NOT_STARTED'].includes(status))) {
+        currentObjective.type = 'future'
+      } else if (
+        currentObjective.actions.every(({ status }) =>
+          ['COMPLETED', 'PARTIALLY_COMPLETED', 'ABANDONED'].includes(status)
+        )
+      ) {
+        currentObjective.type = 'closed'
+      }
+
+      const actionsDisplayList = currentObjective.actions.map(action => {
+        const { monthName, year } = getYearMonthFromDate(action.updated)
+
+        const displayStatus = getStatusText(action.status)
+
+        console.log(displayStatus)
+
+        const retVal = [
+          { text: action.description },
+          { text: `${monthName} ${year}`, format: 'numeric' },
+          { text: getStatusText(action.status), format: 'numeric' },
+        ]
+
+        return retVal
+      })
+
+      currentObjective.actionsDisplay = actionsDisplayList
+      return currentObjective
+    })
+
+    objectives = groupBy(objectives, ({ type }) => type)
+
+    return res.render(`${__dirname}/index`, {
+      backUrl,
+      diversity: getCommentText(comments, 'YOUR_RESPONSIVITY'),
+      decisions: getCommentText(comments, 'YOUR_SUMMARY'),
+      needToKnow: getCommentText(comments, 'THEIR_RESPONSIVITY'),
+      comments: getCommentText(comments, 'THEIR_SUMMARY'),
+      objectives,
+    })
   } catch (error) {
-    logger.error(`Could not retrieve sentence plan ${planId} print summary details for ${id}, error: ${error}`)
-    res.render('app/error', { error })
+    const newError = new Error(`An error occurred whilst trying to print the sentence plan. ${error}`)
+    logger.error(newError)
+    return res.render('app/error', { error: newError })
   }
 }
 
