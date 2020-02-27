708038bd989d86610a8a727d54cd656d8876a45e
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2020-02-27 12:25:21 +0000

    SP-183 show completed plans list

--- app/plans/get.controller.js
+++ app/plans/get.controller.js
@@ -2,7 +2,7 @@ const { logger } = require('../../common/logging/logger')
 const { getSentencePlanSummary } = require('../../common/data/sentencePlanningApi')
 const { isEmptyObject } = require('../../common/utils/util')
 
-const getPlan = plans => {
+const getPlan = (plans = {}) => {
   if (isEmptyObject(plans)) return {}
   return (
     plans.find(({ completedDate }) => completedDate === '' || completedDate === undefined || completedDate === null) ||
@@ -17,24 +17,31 @@ const getCompletedPlans = plans => {
   )
 }
 
+const getPlanType = (plan = {}) => {
+  let planType = 'active'
+  if (isEmptyObject(plan)) {
+    planType = 'none'
+  } else if (plan.draft) {
+    planType = 'draft'
+  }
+  return planType
+}
+
 const sentencePlanSummary = async ({ tokens, params: { id } }, res) => {
   try {
     const plans = await getSentencePlanSummary(id, tokens)
-    const renderInfo = {}
-    renderInfo.individualId = id
-    renderInfo.currentPlan = getPlan(plans)
-    renderInfo.planType = 'active'
-    if (isEmptyObject(renderInfo.currentPlan)) {
-      renderInfo.planType = 'none'
-    } else if (renderInfo.currentPlan.draft) {
-      renderInfo.planType = 'draft'
-    }
+    const currentPlan = getPlan(plans)
 
-    res.render(`${__dirname}/index`, { renderInfo, completedPlans: getCompletedPlans(plans) })
+    res.render(`${__dirname}/index`, {
+      individualId: id,
+      currentPlan,
+      planType: getPlanType(currentPlan),
+      completedPlans: getCompletedPlans(plans),
+    })
   } catch (error) {
     logger.error(`Could not retrieve sentence plan summary for ${id}, error: ${error}`)
     res.render('app/error', { error })
   }
 }
 
-module.exports = { sentencePlanSummary }
+module.exports = { sentencePlanSummary, getPlan, getCompletedPlans, getPlanType }
